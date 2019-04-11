02ed39fa7b40fbb049c21557d8b61faccdccd952
bo wang <986162084@qq.com>
2019-04-11 17:22:40 +0800

    搭建基础框架

---
+++ Fried-Chicken-Calculator/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Fried_Chicken_Calculator.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string UserNumber { get; set; }
+        public double UserMoney { get; set; }
+        
+
+    }
+    public class UserDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
