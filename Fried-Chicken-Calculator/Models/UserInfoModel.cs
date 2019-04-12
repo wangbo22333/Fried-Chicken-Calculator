b904ae94e02bf2150c46216f6925524510ac2325
bo wang <986162084@qq.com>
2019-04-12 13:48:48 +0800

    新增数据库种子。

--- Fried-Chicken-Calculator/Models/UserInfoModel.cs
+++ Fried-Chicken-Calculator/Models/UserInfoModel.cs
@@ -26,8 +26,87 @@ namespace Fried_Chicken_Calculator.Models
 
     public class CalculatorDBContext : DbContext
     {
+        public CalculatorDBContext() : base("CalculatorDB")
+        {
+            Database.SetInitializer(new CalculatorDBInitializer());
+        }       
         public DbSet<User> Users { get; set; }
         public DbSet<UserHistory> UserHistories { get; set; }
 
     }
+    public class CalculatorDBInitializer : CreateDatabaseIfNotExists<CalculatorDBContext>
+    {
+        protected override void Seed(CalculatorDBContext context)
+        {
+            IList<User> defusers = new List<User>();
+            IList<UserHistory> defuserHistories = new List<UserHistory>();
+            defusers.Add(
+                new User
+                {
+                    ID = 1,
+                    UserName = "wangbo",
+                    UserPassword = "123456",
+                    UserNumber = "1001",
+                    UserMoney = 9999
+                }
+                );
+            defusers.Add(
+                new User
+                {
+                    ID = 2,
+                    UserName = "fangmx",
+                    UserPassword = "111111",
+                    UserNumber = "1002",
+                    UserMoney = 1000
+                }
+                );
+            defusers.Add(
+                new User
+                {
+                    ID = 3,
+                    UserName = "wwd123",
+                    UserPassword = "222222",
+                    UserNumber = "1003",
+                    UserMoney = 5000
+                }
+                );
+            context.Users.AddRange(defusers);
+            defuserHistories.Add(
+                new UserHistory
+                {
+                    ID = 1,
+                    UserNumber = "1001",
+                    History = "1+1=2",
+                }
+                );
+            defuserHistories.Add(
+                new UserHistory
+                {
+                    ID = 2,
+                    UserNumber = "1001",
+                    History = "2+2=4",
+                }
+                );
+            defuserHistories.Add(
+                new UserHistory
+                {
+                    ID = 3,
+                    UserNumber = "1001",
+                    History = "3-1=2",
+                }
+                );
+            defuserHistories.Add(
+                new UserHistory
+                {
+                    ID = 4,
+                    UserNumber = "1001",
+                    History = "10*10=100",
+                }
+                );
+            context.UserHistories.AddRange(defuserHistories);
+            base.Seed(context);
+        }
+        
+    }
+
 }
